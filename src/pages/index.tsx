eb21d8a2c79a409a5ab52f13a700ae935d128fe8
Coding-Jarl <senvisage.spam@gmail.com>
2023-06-29 14:25:41 +0200

    Bip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
-import Layout from '../components/layout'
+import Layout from '@/components/layout'
 import Gallery from '@/components/gallery'
 import Card from '@/components/card'
-import axios from 'axios'
 import { store } from '@/lib/redux/store'
 import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useState, useRef } from 'react'
+import useSWRInfinite from 'swr/infinite'
+import useOnScreen from '@/lib/hooks/useOnScreen'
 
 const siteTitle = 'MtgDeckBuilder - Home'
 
@@ -16,28 +17,32 @@ interface CardData {
   name: string
   imageUrl: string
 }
-interface Props {
-  cards: Array<CardData>
-}
 
-export default function Home({ cards }: Props) {
+export default function Home() {
   const [needle, setNeedle] = useState('')
-  const [nextPage, setNextPage] = useState(2)
-  const [cardsToDisplay, setCardsToDisplay] = useState(cards)
+  const bumperRef = useRef()
+  const isBumperVisible = useOnScreen(bumperRef)
+
+  const getKey = (pageIndex: number, previousPageData: Array<CardData>) => {
+    if (previousPageData && !previousPageData.length) return null
+    return `https://api.magicthegathering.io/v1/cards?name=${needle}&page=${
+      pageIndex + 1
+    }`
+  }
+  const fetcher = (url: string) =>
+    fetch(url)
+      .then((r) => r.json())
+      .then((data) => data.cards.filter((card: CardData) => card.imageUrl))
+
+  // data: pages of data from the API
+  // size: number of pages that should be loaded
+  // setSize: setter for "size": +1 => load one more page
+  // getKey: function to get the unique URL for a page
+  // fetcher: function to fetch (sic) data itself
+  const { data, size, setSize } = useSWRInfinite(getKey, fetcher)
 
   const hChangeNeedle = (evt: ChangeEvent<HTMLInputElement>) => {
     setNeedle(evt.currentTarget.value)
-  }
-
-  const hLoad = async () => {
-    const { data } = await axios.get<{ cards: CardData[] }>(
-      `https://api.magicthegathering.io/v1/cards?page=${nextPage}`
-    )
-    const newCards = data.cards.filter((cardData) => cardData.imageUrl)
-
-    setCardsToDisplay([...cardsToDisplay, ...newCards])
-
-    setNextPage(nextPage + 1)
   }
 
   return (
@@ -50,33 +55,26 @@ export default function Home({ cards }: Props) {
         </Head>
         <input type="search" value={needle} onChange={hChangeNeedle} />
         <Gallery>
-          {cardsToDisplay
-            .filter((card) => card.name.includes(needle))
-            .map((cardData) => (
-              <Card
-                name={cardData.name}
-                id={cardData.id}
-                imgUrl={cardData.imageUrl}
-                key={cardData.id}
-              />
-            ))}
+          {!data
+            ? 'Please wait, loading...'
+            : data.map((page) => {
+                return page.map((card: CardData) => (
+                  <Card
+                    name={card.name}
+                    id={card.id}
+                    imgUrl={card.imageUrl}
+                    key={card.id}
+                  />
+                ))
+              })}
         </Gallery>
-        <button onClick={hLoad}>Load More</button>
+        <div ref={bumperRef}>
+          {isBumperVisible && (
+            <button onClick={() => setSize(size + 1)}>Load More</button>
+          )}
+        </div>
       </Layout>
       <ToastContainer />
     </Provider>
   )
-}
-
-export async function getServerSideProps() {
-  const { data } = await axios.get<{ cards: CardData[] }>(
-    'https://api.magicthegathering.io/v1/cards'
-  )
-  const cards = data.cards.filter((cardData) => cardData.imageUrl)
-
-  return {
-    props: {
-      cards,
-    },
-  }
 }
